08dddec7e9cae66349d523cca58a28302a3b0460
beerwithstraw <agarwalp@tcd.ie>
2021-02-19 21:17:37 +0000

    Home Screen Design

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+
+import image from '../assets/images/Saly-1.png'
 
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>HomeScreen</Text>
+            <Image style={styles.image} source={image}/>
+            <Text style={styles.header1}>Welcome to Crypto Wars</Text>
+            <Text style={styles.header2}>Invest your virtual $100,000 and compete with others</Text>
         </View>
     )
 }
@@ -16,7 +20,30 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff',
+        padding: 20,
+    },
+
+    image: {
+        aspectRatio: 1,
+        height: '40%',
+        marginTop: -110,
+        marginBottom: 30
+    }, 
+
+    header1: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+
+    header2: {
+        color: 'gray',
+        textAlign: 'center',
+        fontSize: 15,
+        padding: 15,
+        fontWeight: 'bold'
+
     }
 })
 
